762f5ab39d11d11c2abb041bb2b98bd9aa577356
andihilya <149997511+andihilya@users.noreply.github.com>
2024-06-03 11:36:15 +0800

    Add files via upload

---
+++ tugaslab2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById("1").innerHTML = "Welcome to my page";
+	document.getElementById("2").innerHTML = "Andi Hilyatul Mar'ah";
+	document.getElementById("3").innerHTML = "Halo, nama saya Hilya.Saya tinggal di Makassar dan senang berinteraksi dengan orang baru. Saya memiliki minat dalam membaca, menulis, dan mengeksplorasi berbagai hal baru. Saya juga suka menghabiskan waktu di alam dan menikmati keindahan alam yang ada di sekitar saya. Saya percaya bahwa setiap hari adalah kesempatan untuk belajar dan tumbuh sebagai individu. Terima kasih sudah ingin mengenal saya lebih jauh!";
+}
